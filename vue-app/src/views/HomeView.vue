bf05e40762438188c6fde09ff5d0b7f82cc5899c
Sebastian Liebentraut <basti.zfjk@posteo.de>
2023-03-07 17:01:39 +0000

    erased default vue look

--- vue-app/src/views/HomeView.vue
+++ vue-app/src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-3xl">This is the Home</h1>
 </template>
