84471f7742c91c381529aa9a4fe3b164c4327461
Sebastian Liebentraut <basti.zfjk@posteo.de>
2023-03-15 13:58:27 +0000

    added filter buttons

--- vue-app/src/components/Card/ShowAllCards.vue
+++ vue-app/src/components/Card/ShowAllCards.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <Filter @click="clickHandler" name="English" />
+        <Filter @click="clickHandlerAll" name="All" />
+        <Filter @click="clickHandlerEnglish" name="English" />
+        <Filter @click="clickHandlerGerman" name="German" />
+        <Filter @click="clickHandlerHTML" name="HTML" />
+        <Filter @click="clickHandlerCSS" name="CSS" />
+        <Filter @click="clickHandlerJS" name="JS" />
 
-        <Card v-for="item in cardList" :info="item" />
+        <Card v-for=" item in cardList" :info="item" />
     </div>
 </template>
 
@@ -28,8 +33,38 @@ export default {
         console.log("FETCHED DATA", this.cardList)
     },
     methods: {
-        async clickHandler() {
+        async clickHandlerEnglish() {
             const response = await fetch("http://localhost:3003/ressources/english");
+            const result = await response.json();
+            this.cardList = result.results;
+            console.log("FETCHED DATA", this.cardList)
+        },
+        async clickHandlerGerman() {
+            const response = await fetch("http://localhost:3003/ressources/german");
+            const result = await response.json();
+            this.cardList = result.results;
+            console.log("FETCHED DATA", this.cardList)
+        },
+        async clickHandlerHTML() {
+            const response = await fetch("http://localhost:3003/ressources/html");
+            const result = await response.json();
+            this.cardList = result.results;
+            console.log("FETCHED DATA", this.cardList)
+        },
+        async clickHandlerCSS() {
+            const response = await fetch("http://localhost:3003/ressources/css");
+            const result = await response.json();
+            this.cardList = result.results;
+            console.log("FETCHED DATA", this.cardList)
+        },
+        async clickHandlerJS() {
+            const response = await fetch("http://localhost:3003/ressources/jsc");
+            const result = await response.json();
+            this.cardList = result.results;
+            console.log("FETCHED DATA", this.cardList)
+        },
+        async clickHandlerAll() {
+            const response = await fetch("http://localhost:3003/ressources/");
             const result = await response.json();
             this.cardList = result.results;
             console.log("FETCHED DATA", this.cardList)
